c0056606d543ed94a029a4def76f34db8b8bce9f
전원준 <jwjjwj1960@naver.com>
2025-12-25 10:46:00 +0900

    feat: Application 클래스 및 출석부 파일 추가

---
+++ src/main/java/ui/InputHandler.java
@@ -0,0 +1,10 @@
+package ui;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class InputHandler {
+
+    public static String readInput() {
+        return readLine();
+    }
+}
