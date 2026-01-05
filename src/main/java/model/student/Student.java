cf9f278a17f25949d813ff101cc722ff4a4d12dd
전원준 <jwjjwj1960@naver.com>
2026-01-05 15:11:09 +0900

    feat: student 객체 생성
    Student: nickName, 결석횟수, 지각횟수, 학생상태 저장
    Studnets: Student의 List형태. nickName으로 Student를 찾을 수 있음

--- src/main/java/model/student/Student.java
+++ src/main/java/model/student/Student.java
@@ -1,4 +1,50 @@
 package model.student;
 
+import model.state.StudentState;
+
 public class Student {
+    private static final int ONE = 1;
+    private final String nickName;
+    private int absenceCount;
+    private int lateCount;
+    private StudentState state;
+
+    public Student(String nickName, int absenceCount, int lateCount, StudentState state) {
+        this.nickName = nickName;
+        this.absenceCount = absenceCount;
+        this.lateCount = lateCount;
+        this.state = state;
+    }
+
+    public void increaseAbsenceCount() {
+        this.absenceCount += ONE;
+    }
+
+    public void decreaseAbsenceCount() {
+        this.absenceCount -= ONE;
+    }
+
+    public void increaseLateCount() {
+        this.lateCount += ONE;
+    }
+
+    public void decreaseLateCount() {
+        this.lateCount -= ONE;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public int getAbsenceCount() {
+        return absenceCount;
+    }
+
+    public int getLateCount() {
+        return lateCount;
+    }
+
+    public StudentState getState() {
+        return state;
+    }
 }
--- src/main/java/model/student/Students.java
+++ src/main/java/model/student/Students.java
@@ -1,4 +1,19 @@
 package model.student;
 
+import java.util.List;
+import util.ErrorMessage;
+
 public class Students {
+    private final List<Student> studentList;
+
+    public Students(List<Student> studentList) {
+        this.studentList = studentList;
+    }
+
+    public Student findByNickName(String nickName) {
+        return studentList.stream()
+                .filter(student -> student.getNickName().equals(nickName))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(ErrorMessage.INVALID_NICKNAME_EXIST.getMessage()));
+    }
 }
