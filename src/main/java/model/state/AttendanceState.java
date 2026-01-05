5609cd95610712b9d1ca1a43b986f6708c78a5f4
전원준 <jwjjwj1960@naver.com>
2026-01-05 15:46:54 +0900

    style: State 값 문자열로 상수화
    그냥 정보를 문자 그대로 저장

--- src/main/java/model/state/AttendanceState.java
+++ src/main/java/model/state/AttendanceState.java
@@ -1,17 +1,5 @@
 package model.state;
 
 public enum AttendanceState {
-    NORMAL("출석"),
-    LATE("지각"),
-    ABSENCE("결석");
-
-    private final String attendanceState;
-
-    AttendanceState(String attendanceState) {
-        this.attendanceState = attendanceState;
-    }
-
-    public String getAttendanceState() {
-        return attendanceState;
-    }
+    출석, 지각, 결석
 }
