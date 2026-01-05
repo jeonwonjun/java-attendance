ae6643c5d3b8280ca6fc62dd0b0ee3d4483d1a04
전원준 <jwjjwj1960@naver.com>
2026-01-05 15:45:01 +0900

    feat: FileScanner 추가
    FileScanner로 attendances.csv파일 읽어와 Attendances로 저장
    File Load에 따라서 실패할 경우 ErrorMessage 추가

--- src/main/java/util/ErrorMessage.java
+++ src/main/java/util/ErrorMessage.java
@@ -1,12 +1,13 @@
 package util;
 
 public enum ErrorMessage {
-    IS_HOLIDAY("%s은 등교일이 아닙니다."),
+    IS_HOLIDAY("%s월 %s일 %s은 등교일이 아닙니다."),
     INVALID_NICKNAME_EXIST("등록되지 않은 닉네임입니다."),
     INVALID_INPUT_FORMAT("잘못된 형식을 입력하였습니다."),
     INVALID_IS_ATTENDANCE("이미 출석을 확인하였습니다. 필요한 경우 수정 기능을 이용해 주세요."),
     INVALID_OPEN("캠퍼스 운영 시간에만 출석이 가능합니다."),
-    FUTURE_DAY("아직 수정할 수 없습니다.");
+    FUTURE_DAY("아직 수정할 수 없습니다."),
+    INVALID_FILE_READ("파일을 읽을 수 없습니다.");
 
     private final String message;
     private static final String PREFIX = "ERROR ";
--- src/main/java/util/FileScanner.java
+++ src/main/java/util/FileScanner.java
@@ -1,4 +1,50 @@
 package util;
 
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import model.attendace.Attendance;
+import model.attendace.Attendances;
+
 public class FileScanner {
+    private static final String ATTENDANCES_PATH = "src/main/resources/attendances.csv";
+    private static final String DELIMITER = ",";
+
+    private FileScanner() {
+    }
+
+    public List<Attendance> loadAttendances() {
+        List<String> lines = readFile(ATTENDANCES_PATH);
+        return lines.stream()
+                .map(this::parseToAttendance)
+                .collect(Collectors.toList());
+
+    }
+
+    private Attendance parseToAttendance(String line) {
+        String[] parts = line.split(DELIMITER);
+        return new Attendance(
+                parts[0],
+                LocalDateTime.parse(parts[1])
+        );
+    }
+
+    public List<String> readFile(String filePath) {
+        try {
+            Scanner scanner = new Scanner(new File(filePath));
+            List<String> fileBody = new ArrayList<>();
+            scanner.next();
+            while (scanner.hasNext()) {
+                fileBody.add(scanner.next());
+            }
+            return fileBody;
+        } catch (IOException e) {
+            throw new IllegalStateException(ErrorMessage.INVALID_FILE_READ.getMessage());
+        }
+    }
 }
